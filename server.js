f8881653b6ea36605d104683a8de139f0dc95ad8
jaekook <jaekook4849@gmail.com>
2020-10-17 11:10:04 +0900

    version 1.2.0

--- server.js
+++ server.js
@@ -33,6 +33,8 @@ const server = http.createServer(function(request, response) {
         author.update(request, response);
     } else if(pathname === '/author/update_process') {
         author.update_process(request, response);
+    } else if(pathname === '/author/delete_process') {
+        author.delete_process(request, response);
     } else {
         response.writeHead(404, {'Content-Type' : 'text/html'});
         response.write('Not found');
